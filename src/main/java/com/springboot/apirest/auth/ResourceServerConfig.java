d311f5e6562f34a7033fb3b42191e7db82f0d562
Sergio Esteban <sergio.esteban@navigaglobal.com>
2021-04-11 20:21:59 +0200

    removed some unsed imports

--- src/main/java/com/springboot/apirest/auth/ResourceServerConfig.java
+++ src/main/java/com/springboot/apirest/auth/ResourceServerConfig.java
@@ -1,13 +1,10 @@
 package com.springboot.apirest.auth;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableResourceServer
